0dc587e040efed06614feb747e9e460e351c9d94
Manas Kumar <141910018+manaskumar3003@users.noreply.github.com>
2024-08-24 23:29:11 +0530

    first commit

---
+++ Display-frontend/src/app/page.js
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar"
+import Detail from "@/components/section/landing/detail"
+import Hero from "@/components/section/landing/hero"
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero/>
+      <Detail/>
+    </>
+  )
+}
+
+export default Home
