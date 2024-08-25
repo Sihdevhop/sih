cc58d7922997bb0ce588d84e11d9e6cc991c1b25
Manas Jain <manasdeepakjain@gmail.com>
2024-08-26 01:26:26 +0530

    Profile Responsive

--- frontend/src/app/user/layout.jsx
+++ frontend/src/app/user/layout.jsx
@@ -3,13 +3,13 @@ import UserSidebar from "@/components/UserSidebar"
 const UserLayout = ({ children }) => {
   return (
     <div className="md:grid md:grid-cols-5">
-      <div className="hidden md:block md:col-span-1">
+      <div className="hidden md:block fixed w-1/5 h-screen">
         <UserSidebar />
       </div>
       <div className="md:hidden relative z-10">
         <MobileSidebar/>
       </div>
-      <div className="md:col-span-4 bg-gray-100 ">
+      <div className="md:col-start-2 md:col-span-4 bg-gray-100 ">
         {children}
       </div>
     </div>
